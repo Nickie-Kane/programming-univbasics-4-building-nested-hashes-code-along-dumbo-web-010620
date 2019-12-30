50f1c5ff2f4c7c1149ddfa3fa3275d863d7fe9d9
nickie kane <kameo712@gmail.com>
2019-12-30 00:08:24 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,10 +1,8 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
-      #your key/value pairs here!
+    :montague => {:patriarch => {}, :matriarch => {}, :hero => {}, :heroine_friends => []
       }, 
-    :capulet => {
-      #your key/value pairs here!
+    :capulet => {:patriarch => {}, :matriarch => {}, :hero => {}, :heroine_friends => []
       }}
 
   
